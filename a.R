7f423ac578e3f0dea8a4f3c693fb49302e27a4c9
Philip G <philip.gjedde@ntnu.no>
2023-11-17 10:01:42 +0100

    A commit on the test_branch.

--- a.R
+++ a.R
@@ -1,3 +1,6 @@
 # A whole new wonderful file. Here, we will make a lot of mistakes, and learn!
 
 # More text, for another commit <3
+
+
+# Commit, commit, commit. Now, we are doing this to test our test_branch.
